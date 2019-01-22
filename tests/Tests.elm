72116af61227a28a6f0ffcbb2d4e0305f1315654
Eliot Krone <krondev@pm.me>
2019-01-22 21:35:00 +0300

    elm-format for Test.eml

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -17,6 +17,7 @@ all =
         , test "String.left" <|
             \_ ->
                 Expect.equal "a" (String.left 1 "abcdefg")
+
         -- , test "This test should fail" <|
         --     \_ ->
         --         Expect.fail "failed as expected!"
